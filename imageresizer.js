61bc15493c5e600b433089552518343b6bf12203
Adyrem <42379996+Adyrem@users.noreply.github.com>
2019-12-06 21:48:50 +0100

    Load images from directory
    Added support to load images from locaolhost when using xampp, would also work with servers  Added a little PHP to get all images in a folder on the server(xampp)

--- imageresizer.js
+++ imageresizer.js
@@ -1,45 +1,52 @@
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d");
+
 function loadCanvasWithInputFile(evt) {
-	var canvas = document.getElementById("canvas")
-	var context = canvas.getContext("2d");
-	//var fileinput = document.getElementById("customFile"); // input file
 	var files = evt.target.files; // FileList object
 	var file = files[0];
-
-	var outputArray = new Array(100);
 
 	if (file.type.match('image.*')) {
 		var reader = new FileReader();
 		// Read in the image file as a data URL.
-		var img = new Image();
+		var image = new Image();
 		reader.onload = function (event) {
-			img.onload = function () {
+			image.onload = function () {
 				canvas.width = 100;
 				canvas.height = 100;
-				context.drawImage(img, 0, 0, 100, 100);
-				var imgPixels = context.getImageData(0, 0, 100, 100)
-
-				//https://stackoverflow.com/questions/37174616/js-how-to-turn-image-object-into-grayscale-and-display-it
-				for(var y = 0; y < 100; y++){
-					outputArray[y] = new Array(100)
-					for(var x = 0; x < 100; x++){
-						var i = (y * 4) * 100 + x * 4;
-						var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-						/*imgPixels.data[i] = avg;
-						imgPixels.data[i + 1] = avg;
-						imgPixels.data[i + 2] = avg;*/
-						outputArray[y][x] = avg;
-					}
-
-				}
-				console.log(outputArray)
-
-				context.putImageData(imgPixels, 0, 0)
+				resize(image, context);
 			}
-			img.src = event.target.result;
+			image.src = event.target.result;
 		}
 		reader.readAsDataURL(file);
 
 	} else {
 		alert("not an image");
 	}
+}
+
+function loadFromLocalHost()
+{
+	var image = new Image();
+	image.src = "./img/test.jpg";
+	resize(image, context)
+}
+
+function resize(img, cont) {
+	cont.drawImage(img, 0, 0, 100, 100);
+	var imgPixels = cont.getImageData(0, 0, 100, 100)
+	var outputArray = new Array(100);
+
+	//https://stackoverflow.com/questions/37174616/js-how-to-turn-image-object-into-grayscale-and-display-it
+	for (var y = 0; y < 100; y++) {
+		outputArray[y] = new Array(100)
+		for (var x = 0; x < 100; x++) {
+			var i = (y * 4) * 100 + x * 4;
+			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+			imgPixels.data[i] = avg;
+			imgPixels.data[i + 1] = avg;
+			imgPixels.data[i + 2] = avg;
+			outputArray[y][x] = avg;
+		}
+	}
+	cont.putImageData(imgPixels, 0, 0)
 }
--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 <head>
 	<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"></script>
+	<script>
+	var filenames = <?php echo(json_encode(scandir("./img"))); ?>;
+	filenames.splice(0,2) //get rid of "." and ".." from scandir
+	</script>
 </head>
 
 <body>
@@ -9,14 +14,14 @@
 	<h1>MelanomAI</h1>
 	<p>This Web App uses Neural Networks to detect possible skin diseases</p>
 	<br>
-	<button onclick="">ANALYZE ME!!!</button>
+	<button onclick="loadFromLocalHost()">import from localhost(need e.g. XAMPP)</button>
 
 	<form>
 		<div class="custom-file">
 			<label class="custom-file-label" for="customFile">Choose file</label>
 			<input type="file" class="custom-file-input" id="customFile" onchange="loadCanvasWithInputFile(event)">
 		</div>
-		<input type="submit">
+		<!--<input type="submit"> -->
 	</form>
 
 	<h2 id="content">
@@ -25,6 +30,7 @@
 	<script src="imageresizer.js">
 		
 	</script>
+
 </body>
 
 </html>
